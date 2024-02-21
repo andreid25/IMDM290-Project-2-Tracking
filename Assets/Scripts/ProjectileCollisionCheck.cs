4c04d5c58f9726bae9a32d1e2f1eaba24fa3a2c8
DavinW <49922027+DavinWam@users.noreply.github.com>
2024-02-21 16:59:53 -0500

    added sounds

--- Assets/Scripts/ProjectileCollisionCheck.cs
+++ Assets/Scripts/ProjectileCollisionCheck.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ProjectileCollisionCheck : MonoBehaviour
 {
+    public List<AudioClip> slapSounds;
+    
     // Start is called before the first frame update
     void OnCollisionEnter(Collision col)
     {
+        
+        GetComponent<AudioSource>().clip = slapSounds[(int)UnityEngine.Random.Range(0, 2)];
+        GetComponent<AudioSource>().Play();
         //UnityEngine.Debug.Log("collided");
         ProjectileScript ps =  col.gameObject.GetComponent<ProjectileScript>();
        if(ps)
